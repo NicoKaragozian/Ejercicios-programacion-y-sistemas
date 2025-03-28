de9605d815daa98c77873b8721fe71954a62a2e0
NicoKaragozian <nkaragozian@udesa.edu.ar>
2025-03-27 22:46:49 -0300

    Ejercicio 1

--- semana_02.py
+++ semana_02.py
@@ -1,4 +1,8 @@
-# holagit  
-# guardar local git add git commit git push
+# Ejercicio 1
 
+lista = [1, 2, 3, 4, 5]
 
+def invertir_lista(lista):
+    return lista[::-1]
+
+print(invertir_lista(lista))
