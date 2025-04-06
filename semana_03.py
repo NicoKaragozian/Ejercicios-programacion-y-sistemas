dd2e5e8555e12a0adeee7eedb7b72c665bbce928
NicoKaragozian <nkaragozian@udesa.edu.ar>
2025-04-06 13:04:39 -0300

    Reordenado y commit final

--- semana_03.py
+++ semana_03.py
@@ -1,16 +1,12 @@
 #%%
-#Ejercicio figuritas
 import random 
-import numpy as np
 
 #Datos del problema
 #Álbum con 860 figuritas.
 #Cada figurita se imprime en cantidades iguales y se distribuye aleatoriamente.
 #Cada paquete trae cinco figuritas.
-figus_total = 860
 #Creo la funcion que crea el album vacio
 def crear_album(figus_total):
-    """Crea un album con 860 figuritas, todas en cero."""
     album = [0] * figus_total #Crea una lista de 860 elementos, todos en cero.
     return album
 
@@ -32,7 +28,9 @@ def comprar_figu(figus_total):
 
 #prueba = comprar_figu(figus_total)
 #print(prueba)
+
 #Ejercicio 4
+#Funcion que devuelve la cantidad de figuritas compradas para completar el album
 def cuantas_figus(figus_total):
     album = crear_album(figus_total) #Crea el album vacio
     figuritas_compradas = 0 #Contador de paquetes comprados
@@ -43,36 +41,21 @@ def cuantas_figus(figus_total):
         album[figu] += 1
 
     return figuritas_compradas #Devuelve el contador de figuritas compradas
-    
-#prueba = cuantas_figus(figus_total)
-#print(prueba)
-#Ejercicio 5
-n_repeticiones = 100 #Numero de repeticiones
-figus_total = 860
-
-cant_figus = [cuantas_figus(figus_total) for _ in range(n_repeticiones)] #Lista con los resultados de cada repeticion
-promedio_figus = sum(cant_figus)/ n_repeticiones #Promedio de figuritas compradas
-print(promedio_figus) #Imprime el promedio de figuritas compradas
 
 #Ejercicio 6
+#Funcion que devuelve la cantidad de figuritas compradas para completar el album, dada n_repeticiones
 def experimento_figus(n_repeticiones, figus_total):
     resultado = [cuantas_figus(figus_total) for _ in range(n_repeticiones)] #Lista con los resultados de cada repeticion
     promedio = sum(resultado)/ n_repeticiones #Promedio de figuritas compradas
     return promedio #Devuelve el promedio de figuritas compradas
-    
-figus_total = 860
-n_repeticiones = 100
-#prueba = experimento_figus(n_repeticiones, figus_total)
-#print(prueba)
 
-#Ejercicio 7 y 8
-figus_paquete = 5
-
+#Funcion que devuelve un paquete de tamaño figus_paquete, con figuritas aleatorias dentro del album figus_total
 def comprar_paquete(figus_total, figus_paquete):
     paquete = [comprar_figu(figus_total) for _ in range(figus_paquete)] #Lista con las figuritas del paquete
     return paquete #Devuelve el paquete
 
 #Ejercicio 9
+#Funcion que devuelve la cantidad de paquetes comprados para completar el album
 def cuantos_paquetes(figus_total, figus_paquete):
     album = crear_album(figus_total) #Crea el album vacio
     paquetes_comprados = 0 #Contador de paquetes comprados
@@ -84,19 +67,28 @@ def cuantos_paquetes(figus_total, figus_paquete):
         for figu in paquete:
             album[figu] += 1
     return paquetes_comprados #Devuelve el contador de paquetes comprados
+    
+#prueba = cuantas_figus(figus_total)
+#print(prueba)
 
-figus_total = 860
-figus_paquete = 5
+#Ejercicio 5
+n_repeticiones = 1000 #Numero de repeticiones
+figus_total = 6
+cant_figus = [cuantas_figus(figus_total) for _ in range(n_repeticiones)] #Lista con los resultados de cada repeticion
+promedio_figus = sum(cant_figus)/ n_repeticiones #Promedio de figuritas compradas
+#print(f"Promedio de figuritas compradas para album de 6 figuritas: {promedio_figus}")
 
-#print(cuantos_paquetes(figus_total, figus_paquete))
+#Ejercicio 6  
+n_repeticiones = 100 #Numero de repeticiones
+figus_total = 860 #Numero de figuritas
+cant_figus = experimento_figus (n_repeticiones, figus_total) #Ejecuta el experimento de figuritas
+#print(f"Promedio de figuritas compradas para album de 860 figuritas: {cant_figus}")
 
 #Ejercicio 10
-n_repeticiones = 100 #Numero de repeticiones
+n_repeticiones = 1000
+figus_total = 860
+figus_paquete = 5
 cant_paquetes = [cuantos_paquetes(figus_total, figus_paquete) for _ in range(n_repeticiones)] #Lista con los resultados de cada repeticion
 promedio_paquetes = sum(cant_paquetes)/ n_repeticiones #Promedio de paquetes comprados
-print(promedio_paquetes)  
-
-    
-
-
+#print(f"Promedio de paquetes comprados para album de 860 figuritas: {promedio_paquetes}")
 # %%
